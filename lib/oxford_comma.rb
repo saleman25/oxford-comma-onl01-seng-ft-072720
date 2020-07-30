ef0118d16f69167f9328a620333ee03c04f95508
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 01:58:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+if array.length == 2 
+ return "#{array[0]} and #{array[1]}"
+
+elsif array.length == 1 
+return array.join
+
+elsif 
+array.length == 3 
+return "#{array[0]}, #{array[1]}, and #{array[2]}"
+
+else
+array.length > 3
+array[-1].insert(0, "and ")
 end
+array.join(", ")
+end
+  
