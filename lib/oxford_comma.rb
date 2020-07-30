0d7092aa6857f16b7cf31dc6e48e019bf6523dfa
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 01:52:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
 
 if array.length == 2 
-  "#{array[0]} and #{array[1]}"
+ return "#{array[0]} and #{array[1]}"
 
 elsif 
 array.length == 3 
-"#{array[0]}, #{array[1]}, and #{array[2]}"
+return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
 elsif
 array.lenth > 3
-array[-1].insert(0, "and")
+return array[-1].insert(0, "and")
 end
