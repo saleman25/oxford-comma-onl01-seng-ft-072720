67d206aedb25ee0696cdd5b7dae6862cc8d0a526
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 01:54:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,9 @@ def oxford_comma(array)
 if array.length == 2 
  return "#{array[0]} and #{array[1]}"
 
+elsif array.length == 1 
+return array.
+
 elsif 
 array.length == 3 
 return "#{array[0]}, #{array[1]}, and #{array[2]}"
